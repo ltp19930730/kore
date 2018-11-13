6d72d538afb45262b34bdd7e0d9a76ef2c6ca9c3
TeslaSama <tluo4@stevens.edu>
2018-11-12 21:25:37 -0800

    add a test json response for game api

--- examples/REST_API/src/REST_API.c
+++ examples/REST_API/src/REST_API.c
@@ -1,11 +1,54 @@
 #include <kore/kore.h>
 #include <kore/http.h>
 
-int		page(struct http_request *);
+#include <json-c/json.h>
+
+int		api(struct http_request *);
+
+#define JSON_PRINT_FLAGS JSON_C_TO_STRING_PRETTY|JSON_C_TO_STRING_SPACED
 
 int
-page(struct http_request *req)
+api(struct http_request *req)
 {
-	http_response(req, 200, NULL, 0);
-	return (KORE_RESULT_OK);
+	const char *body;
+	struct json_object *games = NULL;
+	struct json_object *status = NULL;
+
+	if (req->method != HTTP_METHOD_POST &&
+			req->method != HTTP_METHOD_GET) {
+		http_response_header(req, "allow", "POST, GET");
+		http_response(req, HTTP_STATUS_METHOD_NOT_ALLOWED, NULL, 0);
+		return (KORE_RESULT_OK);
+	}
+
+	/* generate a test json oject */
+  games = json_object_new_object();
+	if (NULL == games) {
+		printf("new games json object failed.\n");
+		return (KORE_RESULT_OK);
+	}
+
+	status = json_object_new_object();
+	if (NULL == status) {
+		json_object_put(games);
+		printf("new status json object failed.\n");
+		return (KORE_RESULT_OK);
+	}
+	// TODO
+	json_object_object_add(status, "Version", json_object_new_string("1.2.3.4"));
+	json_object_object_add(status, "Domain", json_object_new_string("USA"));
+
+	// TODO
+	json_object_object_add(games, "status", status);
+	json_object_object_add(games, "game", json_object_new_string("DOTA2"));
+
+	body = json_object_to_json_string_ext(games, JSON_PRINT_FLAGS);
+
+  http_response_header(req, "content-type", "application/json");
+
+
+	http_response(req, 200, body, strlen(body));
+	json_object_put(games);
+
+	return KORE_RESULT_OK;
 }
